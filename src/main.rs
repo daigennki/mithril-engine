47c59863697135c219ef58e903bbcc88ab2685f7
daigennki <daigennki@users.noreply.github.com>
2021-04-02 21:15:57 +0900

    Start work on main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+/* ----------------------------------------------------------------------------
+	MithrilEngine Framework (MEF)
+
+	Copyright (c) 2021, daigennki (@daigennki)
+
+	Licensed under the BSD 3-Clause License.
+------------------------------------------------------------------------------ */
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    let org_name = "daigennki";
+    let game_name = "MithrilEngine";
+
+    // get command line arguments
+    let args: Vec<String> = std::env::args().collect();
+
+    let prefPathResult = sdl2::filesystem::pref_path(org_name, game_name);
+    let prefPath = "";
+    match prefPathResult {
+        Ok(s) => {
+            panic!("Using preferences path: {}", s);
+            //prefPath = s;
+        },
+        Err(e) => panic!("Failed to get preferences path! The application might not have permission to it.")
+    }
 }
