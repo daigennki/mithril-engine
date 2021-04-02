a0f64af0cb0a0c18bfb393ade29254ab1e1ed48c
daigennki <daigennki@users.noreply.github.com>
2021-04-02 22:56:30 +0900

    Some more work on main function

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,39 @@
 ------------------------------------------------------------------------------ */
 extern crate sdl2;
 
+fn log_info(s: &str)
+{
+    println!("{}", s);
+    // TODO: print to log file
+}
+fn log_error(e: &str)
+{
+    let formatted_log_str = ["ERROR: ", &e].concat();
+    log_info(&formatted_log_str);
+    let box_result = msgbox::create("MithrilEngine Error", e, msgbox::common::IconType::Error);
+    // TODO: handle box_result
+}
+
 fn main() {
     let org_name = "daigennki";
     let game_name = "MithrilEngine";
 
     // get command line arguments
-    let args: Vec<String> = std::env::args().collect();
+    // let args: Vec<String> = std::env::args().collect();
 
-    let prefPathResult = sdl2::filesystem::pref_path(org_name, game_name);
-    let prefPath = "";
-    match prefPathResult {
+    // get preferences path (config and save data files will be saved here)
+    let pref_path_result = sdl2::filesystem::pref_path(org_name, game_name);
+    //let mut pref_path_str = String::new();
+    match pref_path_result {
         Ok(s) => {
-            panic!("Using preferences path: {}", s);
-            //prefPath = s;
+            let info_str = ["Using preferences path: ", &s].concat();
+            log_info(&info_str);
+            //pref_path_str = s;
         },
-        Err(e) => panic!("Failed to get preferences path! The application might not have permission to it.")
+        Err(e) => {
+            let error_str = ["Failed to get preferences path: ", &e.to_string()].concat();
+            log_error(&error_str);
+            return;
+        }
     }
 }
