4c31802d945f485296df85a35849383bfab50a3d
daigennki <daigennki@users.noreply.github.com>
2021-04-06 21:38:18 +0900

    Added logging functions

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,26 @@
 
 	Licensed under the BSD 3-Clause License.
 ------------------------------------------------------------------------------ */
+use std::io::Write;
 
-fn log_info(s: &str) {
-    println!("{}", s);
-    // TODO: print to log file
-}
-fn log_error(e: &str) {
-    let formatted_log_str = ["ERROR: ", &e].concat();
-    log_info(&formatted_log_str);
-    let box_result = msgbox::create("MithrilEngine Error", e, msgbox::common::IconType::Error);
-    match box_result {
-        Ok(r) => r,
-        Err(mbe) => {
-            let mbe_str = ["Error occurred while trying to create error message box: ", &mbe.to_string()].concat();
-            log_info(&mbe_str);
-        }
+fn log_info(mut log_file: &std::fs::File, s: &str) {
+    println!("{}", &s);
+    let str_with_newline = format!("{}\n", s);
+    match log_file.write_all(str_with_newline.as_bytes()) {
+        Ok(()) => (),
+        Err(e) => println!("log_info failed to print to log file: {}", e.to_string())
     }
+}
+fn print_error_unlogged(s: &str) {
+    println!("{}", &s);
+    match msgbox::create("MithrilEngine Error", &s, msgbox::common::IconType::Error) {
+        Ok(r) => r,
+        Err(mbe) => println!("msgbox::create failed: {}", &mbe.to_string())
+    }
+}
+
+fn logged_main(/*log_file: &std::fs::File*/) -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
 }
 
 fn main() {
@@ -30,19 +34,49 @@ fn main() {
     // get command line arguments
     // let args: Vec<String> = std::env::args().collect();
 
-    // get preferences path (config and save data files will be saved here)
-    let pref_path_result = sdl2::filesystem::pref_path(org_name, game_name);
-    //let mut pref_path_str = String::new();
-    match pref_path_result {
+    // get preferences path (log, config, and save data files will be saved here)
+    let pref_path_str;
+    match sdl2::filesystem::pref_path(org_name, game_name) {
         Ok(s) => {
-            let info_str = ["Using preferences path: ", &s].concat();
-            log_info(&info_str);
-            //pref_path_str = s;
+            println!("Using preferences path: {}", &s);
+            pref_path_str = s;
         },
         Err(e) => {
-            let error_str = ["Failed to get preferences path: ", &e.to_string()].concat();
-            log_error(&error_str);
+            let e_str = format!("Failed to get preferences path: {}", &e.to_string());
+            print_error_unlogged(&e_str);
             return;
         }
     }
+
+    // open log file
+    let log_file_path = [&pref_path_str, "game.log"].concat();
+    let log_file: std::fs::File;
+    match std::fs::File::create(log_file_path) {
+        Ok(f) => log_file = f,
+        Err(e) => {
+            let e_str = format!("Failed to create log file: {}", &e.to_string());
+            print_error_unlogged(&e_str);
+            return;
+        }
+    }
+
+    // print start date and time
+    log_info(&log_file, &format!("--- INIT {} ---", chrono::Local::now().to_rfc2822()));
+
+    match logged_main(/*&log_file*/) {
+        Ok(()) => (),
+        Err(e) => {
+            log_info(&log_file, &format!("ERROR: {}", &e.to_string()));
+            match msgbox::create("MithrilEngine Error", &e.to_string(), msgbox::common::IconType::Error) {
+                Ok(r) => r,
+                Err(mbe) => {
+                    let mbe_str = ["Error occurred while trying to create error message box: ", &mbe.to_string()].concat();
+                    log_info(&log_file, &mbe_str);
+                }
+            }
+        }
+    }
+
+    // print end date and time
+    log_info(&log_file, &format!("--- EXIT {} ---", chrono::Local::now().to_rfc2822()));
 }
