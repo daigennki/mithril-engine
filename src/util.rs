92860a919f2de9c3f725d1bca54af8aee86b59d8
daigennki <daigennki@users.noreply.github.com>
2021-11-29 11:31:24 +0900

    Code cleanup

--- src/util.rs
+++ src/util.rs
@@ -2,13 +2,10 @@ use std::io::Write;
 
 pub fn log_info(mut log_file: &std::fs::File, s: &str) 
 {
-    println!("{}", &s);
+    println!("{}", s);
     let str_with_newline = format!("{}\n", s);
     match log_file.write_all(str_with_newline.as_bytes()) {
         Ok(()) => (),
-        Err(e) => println!(
-            "log_info failed to print to log file: {}", 
-            e.to_string()
-        )
+        Err(e) => println!("log_info failed to print to log file: {}", &e.to_string())
     }
 }
