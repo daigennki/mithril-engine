09a7d9c4311c74d564a05c417a8b6a2ef96ea0e9
daigennki <daigennki@users.noreply.github.com>
2022-01-26 14:57:51 +0900

    Disabled UI geometry shader for now

--- shaders/src/genspv.sh
+++ shaders/src/genspv.sh
@@ -1,5 +1,5 @@
 echo "Note: In case of errors, make sure the shader source is plain UTF-8 and does *not* have BOM. glslang doesn't seem to like UTF-8 BOM."
 for shader in "$@"
 do
-    glslangValidator -V110 -e "main" --ssb 0 --sub 4 --stb 8 --sbb 8 -o "../$(basename $shader .hlsl).spv"  "$shader"
+    glslangValidator -V -e "main" --ssb 0 --sub 4 --stb 8 --sbb 8 -o "../$(basename $shader .hlsl).spv"  "$shader"
 done
