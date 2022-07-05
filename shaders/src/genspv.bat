6eb1449ea2269325e30e53a212e77dac7b619af6
daigennki <daigennki@users.noreply.github.com>
2022-07-05 12:33:51 +0900

    Fixed discrepancy between genspv.sh and genspv.bat

--- shaders/src/genspv.bat
+++ shaders/src/genspv.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Note: In case of errors, make sure the shader source is plain UTF-8 and does *not* have BOM. glslang doesn't seem to like UTF-8 BOM.
-FOR %%G IN (%*) DO glslangValidator -V110 -e "main" --ssb 0 --sub 4 --stb 8 --sbb 8 -o "../%%~nG.spv"  "%%G"
+FOR %%G IN (%*) DO glslangValidator -V -e "main" -o "../%%~nG.spv"  "%%G"
 pause
