3f0b648637fef9ede6e3842d8173d214795bce5f
daigennki <daigennki@users.noreply.github.com>
2022-07-19 12:05:36 +0900

    Removed unused shader compilation scripts

--- tools/genspv.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Note: In case of errors, make sure the shader source is plain UTF-8 and does *not* have BOM. glslang doesn't seem to like UTF-8 BOM.
-FOR %%G IN (%*) DO glslangValidator -V -e "main" -o "../%%~nG.spv"  "%%G"
-pause
--- tools/genspv.sh
+++
@@ -1,5 +0,0 @@
-echo "Note: In case of errors, make sure the shader source is plain UTF-8 and does *not* have BOM. glslang doesn't seem to like UTF-8 BOM."
-for shader in "$@"
-do
-    glslangValidator -V -e "main" -o "../$(basename $shader .hlsl).spv"  "$shader"
-done
