945ad3045dcb52c6764c5f45668e8efadec091fa
daigennki <daigennki@users.noreply.github.com>
2022-07-30 15:04:15 +0900

    Build script code cleanup

--- build.rs
+++ build.rs
@@ -4,7 +4,6 @@
 	Copyright (c) 2021-2022, daigennki (@daigennki)
 ----------------------------------------------------------------------------- */
 use std::path::Path;
-use std::ffi::OsStr;
 
 fn main()
 {
@@ -12,38 +11,15 @@ fn main()
 	let compiler = shaderc::Compiler::new().unwrap();
 	let mut options = shaderc::CompileOptions::new().unwrap();
 	options.set_source_language(shaderc::SourceLanguage::HLSL);
-	options.set_include_callback(| src_req, _include_type, src_containing, _include_depth | {
-		let src_req_path = Path::new("./src/shaders/").join(src_req);
-		
-		if src_req_path.is_file() {
-			Ok(shaderc::ResolvedInclude{
-				resolved_name: src_req_path.to_str().unwrap().to_string(),
-				content: String::from_utf8(std::fs::read(&src_req_path).unwrap()).unwrap()
-			})
-		} else {
-			Err(format!("shader source '{}' included from '{}' does not exist", src_req, src_containing))
-		}
-	});
+	options.set_include_callback(shader_src_include_callback);
 
 	let shader_paths = std::fs::read_dir("./src/shaders").unwrap()
 		.map(|d| d.unwrap().path())
-		.filter(|p| p.extension().unwrap_or(OsStr::new("")) == "hlsl");
+		.filter(|p| p.extension().unwrap_or(std::ffi::OsStr::new("")) == "hlsl");
 
 	for shader_path in shader_paths {
 		match detect_shader_stage(&shader_path) {
-			Some(shader_stage) => {
-				let shader_src = String::from_utf8(std::fs::read(&shader_path).unwrap()).unwrap();
-				let shader_file_name = shader_path.file_name().unwrap().to_str().unwrap();
-				
-				let compile_result = 
-					compiler.compile_into_spirv(&shader_src, shader_stage, shader_file_name, "main", Some(&options)).unwrap();
-				
-				let spv_file_name = format!("{}.spv", shader_path.file_stem().unwrap().to_str().unwrap());
-				let output_path = Path::new("./shaders/").join(spv_file_name);
-				//let output_path = Path::new(&std::env::var("OUT_DIR").unwrap()).join(spv_file_name);
-				std::fs::write(&output_path, compile_result.as_binary_u8()).unwrap();
-				println!("wrote {}", output_path.to_str().unwrap());
-			}
+			Some(shader_stage) => compile_shader(&shader_path, shader_stage, &compiler, &options),
 			None => println!("could not detect shader stage for shader '{}', skipping...", shader_path.to_str().unwrap())
 		}
 	}
@@ -54,8 +30,35 @@ fn main()
 	println!("cargo:rerun-if-changed=build.rs");
 }
 
-fn detect_shader_stage(path: &std::path::Path) 
-	-> Option<shaderc::ShaderKind>
+fn compile_shader(path: &Path, stage: shaderc::ShaderKind, compiler: &shaderc::Compiler, options: &shaderc::CompileOptions)
+{
+	let shader_src = String::from_utf8(std::fs::read(&path).unwrap()).unwrap();
+	let shader_file_name = path.file_name().unwrap().to_str().unwrap();
+	
+	let compile_result = compiler.compile_into_spirv(&shader_src, stage, shader_file_name, "main", Some(&options)).unwrap();
+	
+	let spv_file_name = format!("{}.spv", path.file_stem().unwrap().to_str().unwrap());
+	let output_path = Path::new("./shaders/").join(spv_file_name);
+	//let output_path = Path::new(&std::env::var("OUT_DIR").unwrap()).join(spv_file_name);
+	std::fs::write(&output_path, compile_result.as_binary_u8()).unwrap();
+	println!("wrote {}", output_path.to_str().unwrap());
+}
+
+fn shader_src_include_callback(src_req: &str, _include_type: shaderc::IncludeType, src_containing: &str, _include_depth: usize)
+	-> shaderc::IncludeCallbackResult
+{
+	let src_req_path = Path::new("./src/shaders/").join(src_req);
+	let content = String::from_utf8(std::fs::read(&src_req_path)
+		.expect(&format!("failed to read shader source '{}' included from '{}'", src_req, src_containing))
+	).unwrap();
+
+	Ok(shaderc::ResolvedInclude{
+		resolved_name: src_req_path.to_str().unwrap().to_string(),
+		content: content
+	})
+}
+
+fn detect_shader_stage(path: &Path) -> Option<shaderc::ShaderKind>
 {
 	let path_str = path.to_str().unwrap();
 	if path_str.ends_with(".vert.hlsl") {
