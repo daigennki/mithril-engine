df0c7c13e759dfd15a41c5621162401b768a69a8
daigennki <daigennki@users.noreply.github.com>
2022-08-19 12:34:37 +0900

    Reduced redundant material loading in `Mesh`

--- src/component/mesh.rs
+++ src/component/mesh.rs
@@ -3,6 +3,7 @@
 
 	Copyright (c) 2021-2022, daigennki (@daigennki)
 ----------------------------------------------------------------------------- */
+use std::any::TypeId;
 use std::sync::Arc;
 use std::path::{ Path, PathBuf };
 use glam::*;
@@ -23,6 +24,8 @@ pub struct Mesh
 	submeshes: Vec<SubMesh>,
 	#[serde(skip)]
 	gpu_buffers: Vec<Arc<ImmutableBuffer<[u8]>>>,
+	#[serde(skip)]
+	materials: Vec<PBR>,	// Box<dyn Material> doesn't compile for some reason, so we use this for now
 
 	model_path: PathBuf
 }
@@ -38,6 +41,7 @@ impl DeferGpuResourceLoading for Mesh
 		
 		// Load each glTF binary buffer into an `ImmutableBuffer`, from which buffer slices will be created.
 		// This reduces memory fragmentation and transfers between CPU and GPU.
+		// It might also be loading some unnecessary data into the GPU though...
 		let gpu_buf_usage = BufferUsage{
 			vertex_buffer: true,
 			index_buffer: true,
@@ -48,12 +52,28 @@ impl DeferGpuResourceLoading for Mesh
 			self.gpu_buffers.push(render_ctx.new_buffer_from_iter(data_buffer.0, gpu_buf_usage)?);
 		}
 
+		self.materials.reserve(doc.materials().len());
+		for mat in doc.materials() {
+			let mat_path = model_path_cd_rel.parent().unwrap()
+				.join(mat.name().ok_or("glTF mesh material has no name")?)
+				.with_extension("yaml");
+
+			log::info!("Loading material file '{}'...", mat_path.display());
+			let mat_yaml_string = String::from_utf8(std::fs::read(mat_path)?)?;
+			let mut deserialized_mat: PBR = serde_yaml::from_str(&mat_yaml_string)?;
+			deserialized_mat.update_descriptor_set(render_ctx)?;
+			self.materials.push(deserialized_mat);
+		}
+
 		for node in doc.nodes() {
 			if let Some(mesh) = node.mesh() {
 				for prim in mesh.primitives() {
-					self.submeshes.push(
-						SubMesh::from_gltf_primitive(prim, &model_path_cd_rel, &self.gpu_buffers, render_ctx)?
-					);
+					let submesh = SubMesh::from_gltf_primitive(prim, &model_path_cd_rel, &self.gpu_buffers, render_ctx)?;
+					// make sure that the material index isn't out of bounds, so we don't have to do error checking before draw
+					if submesh.material_index() >= self.materials.len() {
+						return Err(format!("Material index {} for submesh is out of bounds!", submesh.material_index()).into())
+					}
+					self.submeshes.push(submesh);
 				}
 			}
 		}
@@ -66,6 +86,8 @@ impl Draw for Mesh
 	fn draw(&self, cb: &mut CommandBuffer<SecondaryAutoCommandBuffer>) -> Result<(), GenericEngineError>
 	{
 		for submesh in &self.submeshes {
+			// it's okay that we use a panic function here, since we already checked the material indices earlier
+			self.materials[submesh.material_index()].bind_descriptor_set(cb)?;
 			submesh.draw(cb)?;
 		}
 		Ok(())
@@ -82,7 +104,7 @@ struct SubMesh
 	vertex_buffers: Vec<Arc<BufferSlice<[f32], ImmutableBuffer<[u8]>>>>,
 	index_buf: IndexBufferVariant,
 	vert_count: u32,
-	material: PBR 	// Box<dyn Material> doesn't compile for some reason, so we use this for now
+	mat_index: usize
 }
 impl SubMesh
 {
@@ -106,25 +128,16 @@ impl SubMesh
 			_ => Err(format!("expected u16 or u32 index buffer, got '{:?}'", indices.data_type()))
 		}?;
 		
-		let mat_path = model_path.parent()
-			.unwrap_or(Path::new("./models/"))
-			.join(prim.material().name().ok_or("glTF mesh material has no name")?)
-			.with_extension("yaml");
-		log::info!("Loading material file '{}'...", mat_path.display());
-		let mat_yaml_string = String::from_utf8(std::fs::read(mat_path)?)?;
-		let mut deserialized_mat: PBR = serde_yaml::from_str(&mat_yaml_string)?;
-		deserialized_mat.update_descriptor_set(render_ctx)?;
-
 		Ok(SubMesh{
 			vertex_buffers: vertex_buffers,
 			index_buf: index_buf,
 			vert_count: vert_count.try_into()?,
-			material: deserialized_mat
+			mat_index: prim.material().index().unwrap_or(0)
 		})
 	}
+
 	pub fn draw(&self, cb: &mut CommandBuffer<SecondaryAutoCommandBuffer>) -> Result<(), GenericEngineError>
 	{
-		self.material.bind_descriptor_set(cb)?;
 		cb.bind_vertex_buffers(0, self.vertex_buffers.clone());
 		match &self.index_buf {
 			IndexBufferVariant::U16(buf) => cb.bind_index_buffer(buf.clone()),
@@ -133,23 +146,28 @@ impl SubMesh
 		cb.draw_indexed(self.vert_count, 1, 0, 0, 0)?;
 		Ok(())
 	}
+
+	pub fn material_index(&self) -> usize
+	{
+		self.mat_index
+	}
 }
 
 fn get_buf_slice<T>(accessor: &gltf::Accessor, gpu_buffers: &Vec<Arc<ImmutableBuffer<[u8]>>>)
 	-> Result<Arc<BufferSlice<[T], ImmutableBuffer<[u8]>>>, GenericEngineError>
 	where [T]: BufferContents
 {
-	if std::any::TypeId::of::<T>() != match accessor.data_type() {
-		DataType::I8 => std::any::TypeId::of::<i8>(),
-		DataType::U8 => std::any::TypeId::of::<u8>(),
-		DataType::I16 => std::any::TypeId::of::<i16>(),
-		DataType::U16 => std::any::TypeId::of::<u16>(),
-		DataType::U32 => std::any::TypeId::of::<u32>(),
-		DataType::F32 => std::any::TypeId::of::<f32>(),
+	if TypeId::of::<T>() != match accessor.data_type() {
+		DataType::I8 => TypeId::of::<i8>(),
+		DataType::U8 => TypeId::of::<u8>(),
+		DataType::I16 => TypeId::of::<i16>(),
+		DataType::U16 => TypeId::of::<u16>(),
+		DataType::U32 => TypeId::of::<u32>(),
+		DataType::F32 => TypeId::of::<f32>(),
 	} {
 		return Err(format!(
 			"expected '{:?}', but given glTF primitive has `{:?}`", 
-			std::any::TypeId::of::<T>(), 
+			TypeId::of::<T>(), 
 			accessor.data_type()
 		).into())
 	}
@@ -161,6 +179,8 @@ fn get_buf_slice<T>(accessor: &gltf::Accessor, gpu_buffers: &Vec<Arc<ImmutableBu
 		.slice(start..end)
 		.ok_or(format!("slice between {} and {} bytes into vertex/index buffer is out of range", start, end))?;
 
+	// This should be a valid conversion as long as the glTF offsets and lengths are valid.
+	// Maybe we should check alignment though, just in case?
 	Ok(unsafe { buf_u8.reinterpret::<[T]>() })
 }
 
