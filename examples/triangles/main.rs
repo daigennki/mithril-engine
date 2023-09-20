30dabef8f4d5f075fffcd286589f75af7b0451d2
daigennki <daigennki@users.noreply.github.com>
2023-09-20 21:49:22 +0900

    Clean up old unused assets

--- examples/triangles/main.rs
+++
@@ -1,5 +0,0 @@
-//#![windows_subsystem = "windows"]	// Specify the "windows" subsystem on Windows to hide the console window.
-fn main()
-{
-	mithrilengine::run_game("daigennki", "Untitled Game Project", "world_1.yaml");
-}
