da5f65e29130a10058c73604b8fce32bb17cdb21
daigennki <daigennki@users.noreply.github.com>
2023-11-03 22:12:19 +0900

    Add more objects to the ferris example

---
+++ examples/ferris/fps_counter.rs
@@ -0,0 +1,32 @@
+use shipyard::{IntoIter, IntoWorkloadSystem, WorkloadSystem, UniqueView, View, ViewMut};
+use serde::Deserialize;
+use mithrilengine::render::RenderContext;
+use mithrilengine::component::{ui, EntityComponent, WantsSystemAdded};
+use mithrilengine_derive::EntityComponent;
+
+// This is an example implementation of a framerate counter.
+
+#[derive(shipyard::Component, Deserialize, EntityComponent)]
+struct FpsCounter;
+impl WantsSystemAdded for FpsCounter
+{
+	fn add_system(&self) -> Option<WorkloadSystem>
+	{
+		Some(update_fps_counter.into_workload_system().unwrap())
+	}
+}
+fn update_fps_counter(
+	render_ctx: UniqueView<RenderContext>,
+	mut texts: ViewMut<ui::text::UIText>,
+	fps_counter: View<FpsCounter>,
+)
+{
+	for (mut text_component, _) in (&mut texts, &fps_counter).iter() {
+		// update the fps counter's text
+		let delta_time = render_ctx.delta().as_secs_f64();
+		let fps = 1.0 / delta_time.max(0.000001);
+		let delta_ms = 1000.0 * delta_time;
+		text_component.text_str = format!("{:.0} fps ({:.1} ms)", fps, delta_ms);
+	}
+}
+
--- examples/ferris/main.rs
+++ examples/ferris/main.rs
@@ -1,4 +1,6 @@
 //#![windows_subsystem = "windows"]	// Specify the "windows" subsystem on Windows to hide the console window.
+mod fps_counter;
+
 fn main()
 {
 	mithrilengine::run_game("daigennki", "Untitled Game Project", "examples/ferris/maps/ferris.yaml");
