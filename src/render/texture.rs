d1ba876b96c6507827e21a899e0e383e438a47d2
daigennki <daigennki@users.noreply.github.com>
2023-11-22 15:03:23 +0900

    Texture code refactoring

--- src/render/texture.rs
+++ src/render/texture.rs
@@ -33,15 +33,7 @@ impl Texture
 	) -> Result<(Self, CopyBufferToImageInfo), GenericEngineError>
 	{
 		// TODO: animated textures using APNG, animated JPEG-XL, or multi-layer DDS
-		log::info!("Loading texture file '{}'...", path.display());
-		let file_ext = path
-			.extension()
-			.ok_or("Could not determine texture file extension!")?
-			.to_str();
-		let (vk_fmt, dim, mip_count, img_raw) = match file_ext {
-			Some("dds") => load_dds(path)?,
-			_ => load_other_format(path)?,
-		};
+		let (vk_fmt, dim, mip_count, img_raw) = load_texture(path)?;
 
 		Self::new_from_slice(memory_allocator, subbuffer_allocator, img_raw.as_slice(), vk_fmt, dim, mip_count)
 	}
@@ -129,35 +121,20 @@ impl CubemapTexture
 		let mut cube_fmt = None;
 		let mut cube_dim = None;
 
-		for face in faces {
-			log::info!("Loading texture file '{}'...", face.display());
+		for face_path in faces {
+			let (face_fmt, face_dim, _, img_raw) = load_texture(&face_path)?;
 
-			let file_ext = face
-				.extension()
-				.ok_or("Could not determine texture file extension!")?
-				.to_str();
-			let (vk_fmt, dim, _, img_raw) = match file_ext {
-				Some("dds") => load_dds(&face)?,
-				_ => load_other_format(&face)?,
-			};
-
-			if let Some(f) = cube_fmt.as_ref() {
-				if *f != vk_fmt {
-					return Err("not all faces of a cubemap have the same format!".into());
-				}
-			} else {
-				cube_fmt = Some(vk_fmt);
+			if face_fmt != *cube_fmt.get_or_insert(face_fmt) {
+				return Err("Not all faces of a cubemap have the same format!".into());
+			}
+			if face_dim != *cube_dim.get_or_insert(face_dim) {
+				return Err("Not all faces of a cubemap have the same dimensions!".into());
 			}
 
-			if let Some(d) = cube_dim.as_ref() {
-				if *d != dim {
-					return Err("not all faces of a cubemap have the same dimensions!".into());
-				}
-			} else {
-				cube_dim = Some(dim);
+			let mip_size = get_mip_size(face_fmt, face_dim[0], face_dim[1]).try_into().unwrap();
+			if combined_data.capacity() == 0 {
+				combined_data.reserve(mip_size * 6);
 			}
-
-			let mip_size = get_mip_size(vk_fmt, dim[0], dim[1]).try_into().unwrap();
 			combined_data.extend(&img_raw[..mip_size]);
 		}
 
@@ -211,47 +188,56 @@ impl CubemapTexture
 	}
 }
 
+fn load_texture(path: &Path) -> Result<(Format, [u32; 2], u32, Vec<u8>), GenericEngineError>
+{
+	log::info!("Loading texture file '{}'...", path.display());
+
+	let file_ext = path
+		.extension()
+		.ok_or("Could not determine texture file extension!")?
+		.to_str();
+
+	match file_ext {
+		Some("dds") => Ok(load_dds(path)?),
+		_ => {
+			// Load other formats such as PNG into an 8bpc sRGB RGBA image.
+			let img = image::io::Reader::open(path)?
+				.decode()?
+				.into_rgba8();
+			Ok((Format::R8G8B8A8_SRGB, img.dimensions().into(), 1, img.into_raw()))
+		}
+	}
+}
 fn load_dds(path: &Path) -> Result<(Format, [u32; 2], u32, Vec<u8>), GenericEngineError>
 {
-	let dds_file = std::fs::File::open(path).or_else(|e| Err(format!("Could not open '{}': {}", path.display(), e)))?;
+	let dds_file = std::fs::File::open(path).map_err(|e| format!("Could not open '{}': {}", path.display(), e))?;
 
 	let dds = ddsfile::Dds::read(dds_file)?;
 	let dds_format = dds
 		.get_dxgi_format()
-		.ok_or("Could not determine DDS image format! Make sure it's in DXGI format.")?;
+		.ok_or("Could not determine DDS image format! Make sure it has a DXGI format.")?;
 
 	let vk_fmt = match dds_format {
 		DxgiFormat::BC1_UNorm_sRGB => Format::BC1_RGBA_SRGB_BLOCK,
 		DxgiFormat::BC4_UNorm => Format::BC4_UNORM_BLOCK,
 		DxgiFormat::BC5_UNorm => Format::BC5_UNORM_BLOCK,
-		// treat BC7_UNorm as sRGB for now since Compressonator doesn't support converting to BC7_UNorm_sRGB
+		// treat BC7_UNorm as sRGB for now since Compressonator doesn't support converting to BC7_UNorm_sRGB,
+		// even though the data itself appears to be in sRGB gamma
 		DxgiFormat::BC7_UNorm => Format::BC7_SRGB_BLOCK,
 		DxgiFormat::BC7_UNorm_sRGB => Format::BC7_SRGB_BLOCK,
 		f => return Err(format!("Unsupported DDS format '{:?}'!", f).into()),
 	};
 	let dim = [ dds.get_width(), dds.get_height() ];
 	let mip_count = dds.get_num_mipmap_levels();
-	let img_raw = dds.data;
 
-	Ok((vk_fmt, dim, mip_count, img_raw))
-}
-
-fn load_other_format(path: &Path) -> Result<(Format, [u32; 2], u32, Vec<u8>), GenericEngineError>
-{
-	let img = image::io::Reader::open(path)?.decode()?;
-
-	let vk_fmt = Format::R8G8B8A8_SRGB; // TODO: other formats such as greyscale
-	let dim = [ img.width(), img.height() ];
-	let img_raw = img.into_rgba8().into_raw();
-
-	Ok((vk_fmt, dim, 1, img_raw))
+	Ok((vk_fmt, dim, mip_count, dds.data))
 }
 
 fn get_mip_size(format: Format, mip_width: u32, mip_height: u32) -> DeviceSize
 {
 	let block_extent = format.block_extent();
 	let block_size = format.block_size();
-	let x_blocks = mip_width.div_ceil(block_extent[0]) as u64;
-	let y_blocks = mip_height.div_ceil(block_extent[1]) as u64;
+	let x_blocks = mip_width.div_ceil(block_extent[0]) as DeviceSize;
+	let y_blocks = mip_height.div_ceil(block_extent[1]) as DeviceSize;
 	x_blocks * y_blocks * block_size
 }
