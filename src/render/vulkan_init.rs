1e7a1b6f329eace0b9ce45ad140fd41dce718637
daigennki <daigennki@users.noreply.github.com>
2024-02-01 13:06:02 +0900

    Clean up Vulkan initialization code

--- src/render/vulkan_init.rs
+++ src/render/vulkan_init.rs
@@ -52,10 +52,10 @@ impl std::fmt::Display for DriverVersion
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result
 	{
 		match self {
-			Self::Nvidia((a, b, c, d)) => write!(f, "{}.{}.{}.{}", a, b, c, d),
+			Self::Nvidia((a, b, c, d)) => write!(f, "{a}.{b}.{c}.{d}"),
 			#[cfg(target_family = "windows")]
-			Self::IntelWindows((a, b)) => write!(f, "{}.{}", a, b),
-			Self::Other((a, b, c)) => write!(f, "{}.{}.{}", a, b, c),
+			Self::IntelWindows((a, b)) => write!(f, "{a}.{b}"),
+			Self::Other((a, b, c)) => write!(f, "{a}.{b}.{c}"),
 		}
 	}
 }
@@ -70,19 +70,13 @@ fn create_vulkan_instance(
 	// We'll need to enable the `enumerate_portability` extension if we want to use devices with non-conformant Vulkan
 	// implementations like MoltenVK. For now, we can go without it.
 	let wanted_extensions = InstanceExtensions {
-		khr_get_surface_capabilities2: true,
 		ext_swapchain_colorspace: true,
 		..Default::default()
 	};
 	let enabled_extensions = wanted_extensions
 		.intersection(lib.supported_extensions())
 		.union(&Surface::required_extensions(event_loop));
-
-	log::info!("Enabling instance extensions:");
-	enabled_extensions
-		.into_iter()
-		.filter_map(|(name, enabled)| enabled.then_some(name))
-		.for_each(|ext| log::info!("- {}", ext));
+	log::info!("Enabling instance extensions: {:?}", enabled_extensions);
 
 	// only use the validation layer in debug builds
 	#[cfg(debug_assertions)]
@@ -91,8 +85,8 @@ fn create_vulkan_instance(
 	let enabled_layers = Vec::new();
 
 	let mut inst_create_info = InstanceCreateInfo {
-		application_name: Some(game_name.to_string()),
-		engine_name: Some("MithrilEngine".to_string()),
+		application_name: Some(game_name.into()),
+		engine_name: Some("MithrilEngine".into()),
 		max_api_version: Some(vulkano::Version::V1_3),
 		enabled_layers,
 		enabled_extensions,
@@ -100,75 +94,77 @@ fn create_vulkan_instance(
 	};
 	inst_create_info.engine_version = inst_create_info.application_version;
 
-	let instance = vulkano::instance::Instance::new(lib, inst_create_info)
-		.map_err(|e| EngineError::new("failed to create Vulkan instance", e.unwrap()))?;
-	Ok(instance)
+	vulkano::instance::Instance::new(lib, inst_create_info)
+		.map_err(|e| EngineError::new("failed to create Vulkan instance", e.unwrap()))
 }
 
-/// Get the most appropriate GPU, along with whether or not Resizable BAR is enabled on its largest `DEVICE_LOCAL` memory heap.
-fn get_physical_device(vkinst: &Arc<vulkano::instance::Instance>) -> crate::Result<(Arc<PhysicalDevice>, bool)>
+/// Get the most appropriate GPU, along with whether or not buffers can be directly written to.
+fn get_physical_device(vkinst: &Arc<vulkano::instance::Instance>) -> crate::Result<Arc<PhysicalDevice>>
 {
 	log::info!("Available Vulkan physical devices:");
-	let (mut dgpu, mut igpu) = (None, None);
-	let physical_devices = vkinst
+	let (dgpu, igpu) = vkinst
 		.enumerate_physical_devices()
-		.map_err(|e| EngineError::new("failed to enumerate physical devices", e))?;
-	for (i, pd) in physical_devices.enumerate() {
-		let properties = pd.properties();
-		let driver_ver = DriverVersion::new(properties.driver_version, properties.vendor_id);
-		let driver_name = properties.driver_name.as_ref().map_or("unknown driver", |name| name);
+		.map_err(|e| EngineError::new("failed to enumerate physical devices", e))?
+		.enumerate()
+		.fold((None, None), |(dgpu, igpu), (i, pd)| {
+			let properties = pd.properties();
+			let driver_ver = DriverVersion::new(properties.driver_version, properties.vendor_id);
+			let driver_name = properties.driver_name.as_ref().map_or("unknown driver", |name| name);
 
-		log::info!(
-			"{}: {} ({:?}), driver '{}' version {} (Vulkan {})",
-			i,
-			properties.device_name,
-			properties.device_type,
-			driver_name,
-			driver_ver,
-			properties.api_version
-		);
+			log::info!(
+				"{}: {} ({:?}), driver '{}' version {} (Vulkan {})",
+				i,
+				properties.device_name,
+				properties.device_type,
+				driver_name,
+				driver_ver,
+				properties.api_version
+			);
 
-		match properties.device_type {
-			PhysicalDeviceType::DiscreteGpu => {
-				dgpu.get_or_insert((i, pd));
+			match properties.device_type {
+				PhysicalDeviceType::DiscreteGpu => (dgpu.or(Some((i, pd))), igpu),
+				PhysicalDeviceType::IntegratedGpu => (dgpu, igpu.or(Some((i, pd)))),
+				_ => (dgpu, igpu),
 			}
-			PhysicalDeviceType::IntegratedGpu => {
-				igpu.get_or_insert((i, pd));
-			}
-			_ => (),
-		}
-	}
+		});
 
 	// By default, prefer the discrete GPU over the integrated GPU. However, if the "--prefer_igp"
 	// argument was provided, instead prefer the integrated GPU over the discrete GPU.
-	let (i, physical_device) = std::env::args()
+	let (pd_i, physical_device) = std::env::args()
 		.find(|arg| arg == "--prefer_igp")
 		.and_then(|_| igpu.clone().or_else(|| dgpu.clone()))
 		.or_else(|| dgpu.or(igpu))
 		.ok_or("No GPUs were found!")?;
-	log::info!("Using physical device {}: {}", i, physical_device.properties().device_name);
-
-	let mem_properties = physical_device.memory_properties();
+	log::info!("Using physical device {}: {}", pd_i, physical_device.properties().device_name);
 
 	// Print all the memory heaps and their types.
 	log::info!("Memory heaps and their memory types on physical device:");
-	for (mem_heap, i) in mem_properties.memory_heaps.iter().zip(0_u32..) {
+	let mem_properties = physical_device.memory_properties();
+	for (mem_heap, i) in mem_properties.memory_heaps.iter().zip(0..) {
 		let mib = mem_heap.size / (1024 * 1024);
+		log::info!("{i}: {mib} MiB, flags {:?}", mem_heap.flags);
 
-		log::info!("{}: {} MiB, flags {:?}", i, mib, mem_heap.flags);
-
-		for mem_type in mem_properties.memory_types.iter().filter(|t| t.heap_index == i) {
-			log::info!("└ {:?}", mem_type.property_flags);
-		}
+		mem_properties
+			.memory_types
+			.iter()
+			.enumerate()
+			.filter(|(_, t)| t.heap_index == i)
+			.for_each(|(j, mem_type)| log::info!("└ type {j}: {:?}", mem_type.property_flags));
 	}
 
-	// Check if we can write to buffer memory on VRAM directly, as doing so may improve performance.
-	let allow_direct_buffer_access = match physical_device.properties().device_type {
+	Ok(physical_device)
+}
+
+/// Check if we can directly write to buffer memory on VRAM, as doing so may improve performance.
+fn check_direct_buffer_write(physical_device: &Arc<PhysicalDevice>) -> bool
+{
+	match physical_device.properties().device_type {
 		PhysicalDeviceType::DiscreteGpu => {
-			// For discrete GPUs, check that a memory type with the property flags
+			// For discrete GPUs, look for a memory type with the property flags
 			// `DEVICE_LOCAL | HOST_VISIBLE | HOST_COHERENT` belongs to the largest `DEVICE_LOCAL`
 			// heap. This is the case when Resizable BAR is enabled, in which case all of the VRAM
 			// is host-visible, and writes would immediately go across the PCIe interface.
+			let mem_properties = physical_device.memory_properties();
 			mem_properties
 				.memory_types
 				.iter()
@@ -190,17 +186,12 @@ fn get_physical_device(vkinst: &Arc<vulkano::instance::Instance>) -> crate::Resu
 				})
 		}
 
-		// For integrated GPUs, assume that writing directly to buffer memory is always possible and fast enough.
+		// For integrated GPUs, directly writing to buffer memory is always possible.
 		PhysicalDeviceType::IntegratedGpu => true,
 
-		// For other physical device types, assume we can't write directly to buffer memory.
+		// For other physical device types, assume we can't directly write to buffer memory.
 		_ => false,
-	};
-	if allow_direct_buffer_access {
-		log::info!("Enabling direct buffer writes.");
 	}
-
-	Ok((physical_device, allow_direct_buffer_access))
 }
 
 /// Get a graphics queue family and an optional transfer queue family, then genereate queue create infos for each.
@@ -214,20 +205,20 @@ fn get_queue_infos(physical_device: Arc<PhysicalDevice>) -> crate::Result<Vec<Qu
 	}
 
 	// Get the required graphics queue family, and try to get an optional one for async transfers.
-	let graphics = queue_family_properties
+	let (_, graphics) = queue_family_properties
 		.iter()
-		.zip(0_u32..)
-		.find_map(|(q, i)| q.queue_flags.contains(QueueFlags::GRAPHICS).then_some(i))
+		.zip(0..)
+		.find(|(q, _)| q.queue_flags.contains(QueueFlags::GRAPHICS))
 		.ok_or("No graphics queue family found!")?;
 
 	// Get another queue family that is ideally specifically optimized for async transfers,
 	// by means of finding one with the TRANSFER flag set and least number of flags set.
-	let transfer = queue_family_properties
+	let (_, transfer) = queue_family_properties
 		.iter()
-		.zip(0_u32..)
+		.zip(0..)
 		.filter(|(q, i)| *i != graphics && q.queue_flags.contains(QueueFlags::TRANSFER))
 		.min_by_key(|(q, _)| q.queue_flags.count())
-		.map(|(_, i)| i);
+		.unzip();
 
 	if let Some(tq) = transfer {
 		log::info!("Using queue family {} for transfers", tq);
@@ -245,50 +236,40 @@ fn get_queue_infos(physical_device: Arc<PhysicalDevice>) -> crate::Result<Vec<Qu
 	Ok(infos)
 }
 
-/// Set up the Vulkan instance, physical device, logical device, and queue.
-/// Returns a graphics queue (which owns the device), and an optional transfer queue,
-/// along with whether or not Resizable BAR is in use.
+/// Set up the Vulkan instance, logical device, and queue. Returns a graphics queue (which owns the
+/// device) and an optional transfer queue, along with a `bool` indicating whether or not buffers
+/// on the device can be directly written to.
 pub fn vulkan_setup(
 	game_name: &str,
 	event_loop: &winit::event_loop::EventLoop<()>,
 ) -> crate::Result<(Arc<Queue>, Option<Arc<Queue>>, bool)>
 {
 	let vkinst = create_vulkan_instance(game_name, event_loop)?;
-	let (physical_device, rebar_in_use) = get_physical_device(&vkinst)?;
+	let physical_device = get_physical_device(&vkinst)?;
 
-	let wanted_extensions = DeviceExtensions {
-		// currently no device extensions enabled; leaving this struct here for convenience
-		..Default::default()
-	};
-	let enabled_extensions = DeviceExtensions {
-		khr_swapchain: true,
-		..wanted_extensions.intersection(physical_device.supported_extensions())
-	};
-	log::info!("Enabling device extensions:");
-	enabled_extensions
-		.into_iter()
-		.filter_map(|(name, enabled)| enabled.then_some(name))
-		.for_each(|ext| log::info!("- {}", ext));
+	let direct_buffer_write = check_direct_buffer_write(&physical_device);
+	if direct_buffer_write {
+		log::info!("Enabling direct buffer writes.");
+	}
 
 	// The features enabled here are supported by basically any Vulkan device.
-	let enabled_features = vulkano::device::Features {
-		descriptor_binding_variable_descriptor_count: true,
-		descriptor_indexing: true,
-		dynamic_rendering: true,
-		image_cube_array: true,
-		independent_blend: true,
-		multi_draw_indirect: true,
-		runtime_descriptor_array: true,
-		sampler_anisotropy: true,
-		shader_draw_parameters: true,
-		shader_sampled_image_array_non_uniform_indexing: true,
-		texture_compression_bc: true,
-		..Default::default()
-	};
-
 	let dev_create_info = DeviceCreateInfo {
-		enabled_extensions,
-		enabled_features,
+		enabled_extensions: DeviceExtensions {
+			khr_swapchain: true,
+			..Default::default()
+		},
+		enabled_features: vulkano::device::Features {
+			descriptor_binding_variable_descriptor_count: true,
+			descriptor_indexing: true,
+			dynamic_rendering: true,
+			image_cube_array: true,
+			independent_blend: true,
+			runtime_descriptor_array: true,
+			sampler_anisotropy: true,
+			shader_sampled_image_array_non_uniform_indexing: true,
+			texture_compression_bc: true,
+			..Default::default()
+		},
 		queue_create_infos: get_queue_infos(physical_device.clone())?,
 		..Default::default()
 	};
@@ -297,5 +278,5 @@ pub fn vulkan_setup(
 		.map_err(|e| EngineError::new("failed to create Vulkan logical device", e.unwrap()))?;
 	let graphics_queue = queues.next().unwrap();
 	let transfer_queue = queues.next();
-	Ok((graphics_queue, transfer_queue, rebar_in_use))
+	Ok((graphics_queue, transfer_queue, direct_buffer_write))
 }
