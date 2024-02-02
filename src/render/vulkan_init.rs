2a07df9e8202271a0c16a40983ea5fbc02ec30b9
daigennki <daigennki@users.noreply.github.com>
2024-02-02 17:51:53 +0900

    Set engine version instead of app version for Vulkan
    Also cleaned up the Vulkan init code a little more.

--- src/render/vulkan_init.rs
+++ src/render/vulkan_init.rs
@@ -8,9 +8,10 @@
 use std::sync::Arc;
 use vulkano::device::{
 	physical::{PhysicalDevice, PhysicalDeviceType},
-	DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo, QueueFlags,
+	Device, DeviceCreateInfo, DeviceExtensions, Features, Queue, QueueCreateInfo, QueueFlags,
 };
 use vulkano::instance::{Instance, InstanceCreateInfo, InstanceExtensions};
+use vulkano::library::VulkanLibrary;
 use vulkano::memory::MemoryPropertyFlags;
 use vulkano::swapchain::Surface;
 use vulkano::DeviceSize;
@@ -49,42 +50,6 @@ impl std::fmt::Display for DriverVersion
 	}
 }
 
-/// Create a vulkan instance, then get an appropriate physical device.
-fn create_vulkan_instance(game_name: &str, event_loop: &EventLoop<()>) -> crate::Result<Arc<Instance>>
-{
-	let lib = vulkano::library::VulkanLibrary::new().map_err(|e| EngineError::new("failed to load Vulkan library", e))?;
-
-	// VK_KHR_portability_enumeration needs to be enabled if we want to support non-conformant
-	// Vulkan implementations like MoltenVK. For now, we can go without it.
-	let wanted_extensions = InstanceExtensions {
-		ext_swapchain_colorspace: true,
-		..Default::default()
-	};
-	let enabled_extensions = wanted_extensions
-		.intersection(lib.supported_extensions())
-		.union(&Surface::required_extensions(event_loop));
-	log::info!("Enabling instance extensions: {:?}", enabled_extensions);
-
-	// only use the validation layer in debug builds
-	#[cfg(debug_assertions)]
-	let enabled_layers = vec!["VK_LAYER_KHRONOS_validation".into()];
-	#[cfg(not(debug_assertions))]
-	let enabled_layers = Vec::new();
-
-	let mut inst_create_info = InstanceCreateInfo {
-		application_name: Some(game_name.into()),
-		engine_name: Some("MithrilEngine".into()),
-		max_api_version: Some(vulkano::Version::V1_3),
-		enabled_layers,
-		enabled_extensions,
-		..InstanceCreateInfo::application_from_cargo_toml()
-	};
-	inst_create_info.engine_version = inst_create_info.application_version;
-
-	vulkano::instance::Instance::new(lib, inst_create_info)
-		.map_err(|e| EngineError::new("failed to create Vulkan instance", e.unwrap()))
-}
-
 fn get_physical_device(vkinst: &Arc<Instance>) -> crate::Result<Arc<PhysicalDevice>>
 {
 	let physical_devices: smallvec::SmallVec<[_; 4]> = vkinst
@@ -106,46 +71,148 @@ fn get_physical_device(vkinst: &Arc<Instance>) -> crate::Result<Arc<PhysicalDevi
 
 	// Prefer an integrated GPU if the "--prefer_igp" argument was provided. Otherwise, prefer a
 	// discrete GPU.
-	let dgpu = physical_devices
+	let dgpu_i = physical_devices
 		.iter()
 		.position(|pd| pd.properties().device_type == PhysicalDeviceType::DiscreteGpu);
-	let igpu = physical_devices
+	let igpu_i = physical_devices
 		.iter()
 		.position(|pd| pd.properties().device_type == PhysicalDeviceType::IntegratedGpu);
 	let pd_i = std::env::args()
 		.find(|arg| arg == "--prefer_igp")
-		.and_then(|_| igpu.or(dgpu))
-		.or_else(|| dgpu.or(igpu))
+		.and_then(|_| igpu_i.or(dgpu_i))
+		.or_else(|| dgpu_i.or(igpu_i))
 		.ok_or("No GPUs were found!")?;
-	let physical_device = physical_devices[pd_i].clone();
+	let physical_device = physical_devices.into_iter().nth(pd_i).unwrap();
 
 	let pd_api_ver = physical_device.properties().api_version;
 	log::info!("Using physical device {pd_i} (Vulkan {pd_api_ver})");
 
-	log::info!("Memory heaps:");
 	let mem_properties = physical_device.memory_properties();
 	for (i, mem_heap) in mem_properties.memory_heaps.iter().enumerate() {
 		let mib = mem_heap.size / (1024 * 1024);
-		log::info!("{i}: {mib} MiB");
+		log::info!("Memory heap {i}: {mib} MiB");
 	}
-
-	log::info!("Memory types:");
 	for (i, mem_type) in mem_properties.memory_types.iter().enumerate() {
-		log::info!("{i}: heap {}, {:?}", mem_type.heap_index, mem_type.property_flags);
+		log::info!("Memory type {i}: heap {}, {:?}", mem_type.heap_index, mem_type.property_flags);
 	}
 
 	Ok(physical_device)
 }
 
-/// Check if we can directly write to buffer memory on VRAM, as doing so may improve performance.
-fn check_direct_buffer_write(physical_device: &Arc<PhysicalDevice>) -> bool
+// The features enabled here are supported by basically any Vulkan device on PC.
+const ENABLED_FEATURES: Features = Features {
+	descriptor_binding_variable_descriptor_count: true,
+	descriptor_indexing: true,
+	dynamic_rendering: true,
+	image_cube_array: true,
+	independent_blend: true,
+	runtime_descriptor_array: true,
+	sampler_anisotropy: true,
+	shader_sampled_image_array_non_uniform_indexing: true,
+	texture_compression_bc: true,
+	..Features::empty()
+};
+
+fn create_logical_device(physical_device: Arc<PhysicalDevice>) -> crate::Result<(Arc<Queue>, Option<Arc<Queue>>)>
 {
-	match physical_device.properties().device_type {
+	let queue_family_properties = physical_device.queue_family_properties();
+	for (i, q) in queue_family_properties.iter().enumerate() {
+		log::info!("Queue family {i}: {} queue(s), {:?}", q.queue_count, q.queue_flags);
+	}
+
+	// Look for a graphics queue family and an optional (preferably dedicated) transfer queue
+	// family, and generate `QueueCreateInfo`s for one queue on each.
+	let (_, graphics_qfi) = queue_family_properties
+		.iter()
+		.zip(0..)
+		.find(|(q, _)| q.queue_flags.contains(QueueFlags::GRAPHICS))
+		.ok_or("No graphics queue family found!")?;
+
+	// For transfers, try to find a queue family with the TRANSFER flag and least number of flags.
+	let (_, transfer_qfi) = queue_family_properties
+		.iter()
+		.zip(0..)
+		.filter(|(q, i)| *i != graphics_qfi && q.queue_flags.contains(QueueFlags::TRANSFER))
+		.min_by_key(|(q, _)| q.queue_flags.count())
+		.unzip();
+	if let Some(tq) = transfer_qfi {
+		log::info!("Using queue family {} for transfers", tq);
+	}
+
+	let queue_create_infos = [graphics_qfi]
+		.into_iter()
+		.chain(transfer_qfi)
+		.map(|queue_family_index| QueueCreateInfo {
+			queue_family_index,
+			..Default::default()
+		})
+		.collect();
+
+	let dev_info = DeviceCreateInfo {
+		enabled_extensions: DeviceExtensions {
+			khr_swapchain: true,
+			..Default::default()
+		},
+		enabled_features: ENABLED_FEATURES,
+		queue_create_infos,
+		..Default::default()
+	};
+	let (_, mut queues) = Device::new(physical_device, dev_info)
+		.map_err(|e| EngineError::new("failed to create Vulkan logical device", e.unwrap()))?;
+
+	let graphics_queue = queues.next().unwrap();
+	let transfer_queue = queues.next();
+	Ok((graphics_queue, transfer_queue))
+}
+
+/// Set up the Vulkan instance, logical device, and queue. Returns a graphics queue (which owns the
+/// device) and an optional transfer queue, along with a `bool` indicating whether or not buffers
+/// on the device can be directly written to.
+pub fn vulkan_setup(app_name: &str, event_loop: &EventLoop<()>) -> crate::Result<(Arc<Queue>, Option<Arc<Queue>>, bool)>
+{
+	let lib = VulkanLibrary::new().map_err(|e| EngineError::new("failed to load Vulkan library", e))?;
+
+	// only use the validation layer in debug builds
+	#[cfg(debug_assertions)]
+	let enabled_layers = vec!["VK_LAYER_KHRONOS_validation".into()];
+	#[cfg(not(debug_assertions))]
+	let enabled_layers = Vec::new();
+
+	const OPTIONAL_INSTANCE_EXTENSIONS: InstanceExtensions = InstanceExtensions {
+		ext_swapchain_colorspace: true,
+		..InstanceExtensions::empty()
+	};
+	// TODO: take app version as parameter
+	let mut inst_create_info = InstanceCreateInfo {
+		application_name: Some(app_name.into()),
+		engine_name: Some(env!("CARGO_PKG_NAME").into()),
+		engine_version: vulkano::Version {
+			major: env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap(),
+			minor: env!("CARGO_PKG_VERSION_MINOR").parse().unwrap(),
+			patch: env!("CARGO_PKG_VERSION_PATCH").parse().unwrap(),
+		},
+		enabled_layers,
+		enabled_extensions: OPTIONAL_INSTANCE_EXTENSIONS
+			.intersection(lib.supported_extensions())
+			.union(&Surface::required_extensions(event_loop)),
+		..Default::default()
+	};
+	inst_create_info.engine_version = inst_create_info.application_version;
+
+	log::info!("Enabling instance extensions: {:?}", &inst_create_info.enabled_extensions);
+
+	let vkinst =
+		Instance::new(lib, inst_create_info).map_err(|e| EngineError::new("failed to create Vulkan instance", e.unwrap()))?;
+
+	let physical_device = get_physical_device(&vkinst)?;
+
+	// Check if we can directly write to device-local buffer memory, as doing so may be faster.
+	let direct_buffer_write = match physical_device.properties().device_type {
 		PhysicalDeviceType::IntegratedGpu => true, // Always possible for integrated GPU.
 		PhysicalDeviceType::DiscreteGpu => {
-			// For discrete GPUs, check if Resizable BAR is enabled by looking for a host-visible
-			// memory type that belongs to a device-local heap larger than **exactly** 256 **MiB**.
-			const REBAR_THRESHOLD: DeviceSize = 256 * 1024 * 1024;
+			// For discrete GPUs, look for a host-visible memory type belonging to a device-local
+			// heap larger than **exactly** 256 **MiB**.
+			const DIRECT_WRITE_THRESHOLD: DeviceSize = 256 * 1024 * 1024;
 			let mem_properties = physical_device.memory_properties();
 			mem_properties
 				.memory_types
@@ -157,95 +224,14 @@ fn check_direct_buffer_write(physical_device: &Arc<PhysicalDevice>) -> bool
 							| MemoryPropertyFlags::HOST_COHERENT,
 					)
 				})
-				.any(|t| mem_properties.memory_heaps[t.heap_index as usize].size > REBAR_THRESHOLD)
+				.any(|t| mem_properties.memory_heaps[t.heap_index as usize].size > DIRECT_WRITE_THRESHOLD)
 		}
 		_ => unreachable!(),
-	}
-}
-
-/// Get a graphics queue family and an optional transfer queue family, then genereate queue create infos for each.
-fn get_queue_infos(physical_device: Arc<PhysicalDevice>) -> crate::Result<Vec<QueueCreateInfo>>
-{
-	let queue_family_properties = physical_device.queue_family_properties();
-
-	log::info!("Queue families:");
-	for (i, q) in queue_family_properties.iter().enumerate() {
-		log::info!("{}: {} queue(s), {:?}", i, q.queue_count, q.queue_flags);
-	}
-
-	// Get the required graphics queue family.
-	let (_, graphics) = queue_family_properties
-		.iter()
-		.zip(0..)
-		.find(|(q, _)| q.queue_flags.contains(QueueFlags::GRAPHICS))
-		.ok_or("No graphics queue family found!")?;
-
-	// Get another queue family that is ideally specifically optimized for async transfers,
-	// by means of finding one with the TRANSFER flag set and least number of flags set.
-	let (_, transfer) = queue_family_properties
-		.iter()
-		.zip(0..)
-		.filter(|(q, i)| *i != graphics && q.queue_flags.contains(QueueFlags::TRANSFER))
-		.min_by_key(|(q, _)| q.queue_flags.count())
-		.unzip();
-
-	if let Some(tq) = transfer {
-		log::info!("Using queue family {} for transfers", tq);
-	}
-
-	let infos = [graphics]
-		.into_iter()
-		.chain(transfer)
-		.map(|queue_family_index| QueueCreateInfo {
-			queue_family_index,
-			..Default::default()
-		})
-		.collect();
-
-	Ok(infos)
-}
-
-/// Set up the Vulkan instance, logical device, and queue. Returns a graphics queue (which owns the
-/// device) and an optional transfer queue, along with a `bool` indicating whether or not buffers
-/// on the device can be directly written to.
-pub fn vulkan_setup(
-	game_name: &str,
-	event_loop: &winit::event_loop::EventLoop<()>,
-) -> crate::Result<(Arc<Queue>, Option<Arc<Queue>>, bool)>
-{
-	let vkinst = create_vulkan_instance(game_name, event_loop)?;
-	let physical_device = get_physical_device(&vkinst)?;
-
-	let direct_buffer_write = check_direct_buffer_write(&physical_device);
+	};
 	if direct_buffer_write {
 		log::info!("Enabling direct buffer writes.");
 	}
 
-	// The features enabled here are supported by basically any Vulkan device.
-	let dev_create_info = DeviceCreateInfo {
-		enabled_extensions: DeviceExtensions {
-			khr_swapchain: true,
-			..Default::default()
-		},
-		enabled_features: vulkano::device::Features {
-			descriptor_binding_variable_descriptor_count: true,
-			descriptor_indexing: true,
-			dynamic_rendering: true,
-			image_cube_array: true,
-			independent_blend: true,
-			runtime_descriptor_array: true,
-			sampler_anisotropy: true,
-			shader_sampled_image_array_non_uniform_indexing: true,
-			texture_compression_bc: true,
-			..Default::default()
-		},
-		queue_create_infos: get_queue_infos(physical_device.clone())?,
-		..Default::default()
-	};
-
-	let (_, mut queues) = vulkano::device::Device::new(physical_device, dev_create_info)
-		.map_err(|e| EngineError::new("failed to create Vulkan logical device", e.unwrap()))?;
-	let graphics_queue = queues.next().unwrap();
-	let transfer_queue = queues.next();
+	let (graphics_queue, transfer_queue) = create_logical_device(physical_device)?;
 	Ok((graphics_queue, transfer_queue, direct_buffer_write))
 }
