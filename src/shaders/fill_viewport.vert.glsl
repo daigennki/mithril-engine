cbe86cd59d4fcba0068f864373b4d0f0d77b13d9
daigennki <daigennki@users.noreply.github.com>
2024-03-14 13:01:51 +0900

    Clean up some more shaders

--- src/shaders/fill_viewport.vert.glsl
+++ src/shaders/fill_viewport.vert.glsl
@@ -1,8 +1,6 @@
 #version 460
-
+const vec2 POSITIONS[3] = { { -1.0, -1.0 }, { -1.0, 3.0 }, { 3.0, -1.0 } };
 void main()
 {
-	vec2 texcoords[3] = { { 0.0, 0.0 }, { 0.0, 2.0 }, { 2.0, 0.0 } };
-	vec2 texcoord = texcoords[min(gl_VertexIndex, 2)];
-	gl_Position = vec4(texcoord * 2.0 - 1.0, 0.0, 1.0);
+	gl_Position = vec4(POSITIONS[gl_VertexIndex], 0.0, 1.0);
 }
--- src/shaders/smaa_blend.vert.glsl
+++ src/shaders/smaa_blend.vert.glsl
@@ -2,14 +2,15 @@
 #define SMAA_INCLUDE_VS 1
 #include "smaa_common.glsl"
 
+const vec2 TEXCOORDS[3] = { { 0.0, 0.0 }, { 2.0, 0.0 }, { 0.0, 2.0 } };
+
 layout(location = 0) out vec2 texcoord;
 layout(location = 1) out vec2 pixcoord;
 layout(location = 2) out vec4 offset[3];
 
 void main()
 {
-    vec2 texcoords[3] = { { 0.0, 0.0 }, { 2.0, 0.0 }, { 0.0, 2.0 } };
-    texcoord = texcoords[min(gl_VertexIndex,2)];
+    texcoord = TEXCOORDS[gl_VertexIndex];
     gl_Position = vec4(texcoord * 2.0 - 1.0, 0.0, 1.0);
 
     SMAABlendingWeightCalculationVS(texcoord, pixcoord, offset);
